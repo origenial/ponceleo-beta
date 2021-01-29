70065f5910e0e3d0c74cf0b32e14369956525253
lyderichti59 <lyderic.dutillieux@origenial.fr>
2021-01-29 12:43:58 +0100

    Converted subscription email from plaintext to html
    Signed-off-by: lyderichti59 <lyderic.dutillieux@origenial.fr>

--- modules/ponceleo_landing/backend/src/main/ponceleo/landing/backend/subscribe/service.clj
+++ modules/ponceleo_landing/backend/src/main/ponceleo/landing/backend/subscribe/service.clj
@@ -1,7 +1,8 @@
 (ns ponceleo.landing.backend.subscribe.service
   "This namespace defines basic core functions for the subscription services.
   There should not be a single side effect here, only pure functions."
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [hiccup.page :refer [html5]]))
 
 (defn prepare-emails
   "Prepare the emails to send for the subscribtion to be effective"
@@ -9,26 +10,27 @@
   {:mails-to-send
    {:to email
     :bcc admin-email
-    :subject "Inscription à la newsletter de Ponceleo"
+    :subject "Confirmation d'inscription à la newsletter de Ponceleo"
     :body
-    (str/join
-     "\n"
-     ["Madame, Monsieur,"
-      ""
-      "C'est avec plaisir que je confirme votre inscription à la newsletter de Ponceleo !"
-      (str "Dès à présent, vous recevrez (sans spam) à l'adresse "
-           email
-           " :")
-      " - Les avancées de l'application ;"
-      " - Les avancées de l'entreprise ;"
-      " - Les offres commerciales de Ponceleo."
-      ""
-      "Bien cordialement,"
-      ""
-      "Lydéric Dutillieux"
-      "Fondateur de Ponceleo"
-      admin-email
-      ""
-      (str "PS : Pour vous désinscrire d'une de ces options ou de la newsletter, envoyez un mail à l'adresse suivante : " admin-email)
-      ])
-    }})
+    [{:type "text/html; charset=utf-8"
+      :content
+      (html5
+       {:lang "fr"}
+       [:div
+        [:span "Madame, Monsieur,"] [:br]
+        [:p
+         "C'est avec plaisir que je " [:b "confirme votre inscription à la newsletter de Ponceleo !"] [:br]
+         "Dès à présent, vous recevrez (sans spam) à l'adresse " email " :"
+         [:ul
+          [:li "Les avancées de l'application ;"]
+          [:li "Les avancées de l'entreprise ;"]
+          [:li "Les offres commerciales de Ponceleo."]]]
+        [:p "Bien cordialement,"]
+        [:div
+         [:span [:b "Lydéric Dutillieux,"]] [:br]
+         [:span "Fondateur de Ponceleo"] [:br]
+         [:span admin-email]]]
+       [:p
+        "PS : Pour vous désinscrire d'une de ces options ou de la newsletter, "
+        "envoyez un mail à l'adresse suivante : "
+        admin-email])}]}})
